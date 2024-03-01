937c42b2d4fec6e94cbb0f86feef86c2b38fd34a
Mark Rubin <mark.p.rubin@gmail.com>
2024-03-01 11:53:02 -0700

    adds send email function

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+from email.mime.text import MIMEtext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Check for updates
 
@@ -12,3 +17,15 @@ def check_updates():
     
     # Check for updates by comparing to prev. state
     # Send email notification if updates detected
+
+subject = "Email Test Subject"
+body = "This is the body of the text"
+sender = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
+recipients = ["EMAIL"] # myself in this case
+  
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEtext(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
