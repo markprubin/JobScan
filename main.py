ec107c896e46617186fb74409e4f78606176cf55
Mark Rubin <mark.p.rubin@gmail.com>
2024-03-01 18:34:47 -0700

    update send_mail function

--- main.py
+++ main.py
@@ -29,3 +29,10 @@ def send_email(subject, body, sender, recipients, password):
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
+    
+send_email(subject, body, sender, recipients, password)
+
