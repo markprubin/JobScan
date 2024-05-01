0039d519db0c3af690dbdbffcc6f1aa54b673166
Mark Rubin <mark.p.rubin@gmail.com>
2024-05-01 13:52:44 -0700

    stage changes

--- main.py
+++ main.py
@@ -1,61 +1,61 @@
-from re import sub
+import json
+import os
 import requests
 from bs4 import BeautifulSoup
 import smtplib
 from email.mime.text import MIMEText
-from dotenv import load_dotenv
-import os
 import difflib
+from dotenv import load_dotenv
 
 load_dotenv()
 
-# Single URL
-url = os.getenv("URLLIST")
-# List of URLs
-# url_list = os.getenv("URLLIST").split(",") if os.getenv("URLLIST") else []
+def load_urls():
+    with open('urls.json', 'r') as file:
+        return json.load(file)
 
+def check_updates(websites_dict):
+    email_body = ""
+    any_updates = False
 
-# Check for updates
-def check_updates():
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # Targets each instance of an <a> tag on kitman's website
-    content_containers = soup.find_all('a', class_="posting-title")
-    new_content = ""
-    
-    # Targets each instance and displays the text with a break for each <a> tag
-    for container in content_containers:
-        new_content += container.get_text(strip=True) + "<br>"
-    
-    try:
-        with open("updates.txt", "r") as file:
-            old_content = file.read()
-    except FileNotFoundError:
-        old_content = "" # Assume no existing file put in empty string
-
-    differ = difflib.Differ()
-    differences = list(differ.compare(old_content.splitlines(), new_content.splitlines()))
-
-    # Ensures that only real updates are passed, and any whitespace that occurs due to no changes being made does not trigger a "change"
-    updates = [line for line in differences if line.startswith('+') or line.startswith('-') or line.startswith('?')]
-    
-    if updates:
-        update_text = '\n'.join(updates)
-        send_email(subject, update_text, sender, recipients, password)
+    for website_name, config in websites_dict.items():
+        url = config['url']
+        find_all_params = config['find_all_params']
         
-        # Update updates.txt
-        with open("updates.txt", "w") as file:
-            file.write(new_content)
-            
-    else:
-        no_updates_message = "No updates found today."
-        send_email("Daily Job Posting Update - No Changes", no_updates_message, sender, recipients, password)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        content_containers = soup.find_all(**find_all_params)
+        
+        new_content = ""
+        for container in content_containers:
+            new_content += f"{container.get_text(strip=True)}<br>"
 
-subject = "Daily Job Posting Update"
-sender = os.getenv("EMAIL")
-password = os.getenv("PASSWORD")
-recipients = os.getenv("EMAIL").split(",") if os.getenv("EMAIL") else [] # myself in this case
-  
+        # Check for updates.txt file and read its content
+        try:
+            with open("updates.txt", "r") as file:
+                old_content = file.read()
+        except FileNotFoundError:
+            old_content = ""  # If the file doesn't exist, consider it as empty
+
+        # Compare the old content with the new content
+        differ = difflib.Differ()
+        differences = list(differ.compare(old_content.splitlines(), new_content.splitlines()))
+        updates = [line for line in differences if line.startswith('+ ') or line.startswith('- ')]
+
+        if updates:
+            any_updates = True
+            update_text = '\n'.join(updates)
+            email_body += f"{website_name}\n{update_text}\n\n"
+
+    if any_updates:
+        send_email(subject, email_body, sender, recipients, password)
+    else:
+        send_email("Daily Job Posting Update - No Changes", "No updates found today.", sender, recipients, password)
+
+    subject = "Daily Job Posting Update"
+    sender = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+    recipients = os.getenv("EMAIL").split(",") if os.getenv("EMAIL") else [] # myself in this case
+    
 def send_email(subject, body, sender, recipients, password):
     msg = MIMEText(body, 'html')
     msg['Subject'] = subject
@@ -65,11 +65,10 @@ def send_email(subject, body, sender, recipients, password):
         smtp_server.login(sender, password)
         smtp_server.sendmail(sender, recipients, msg.as_string())
     print("Message sent!")
-    
 
 def main():
-    check_updates()
-
+    websites_dict = load_urls()
+    check_updates(websites_dict)
 
 if __name__ == '__main__':
     main()
