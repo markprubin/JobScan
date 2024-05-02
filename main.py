21d0e9d3994884da55495fac2c57bee56cbf5701
Mark Rubin <mark.p.rubin@gmail.com>
2024-05-01 18:12:21 -0700

    scrape adjust changes

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 from email.mime.text import MIMEText
-import difflib
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def load_urls():
     with open('urls.json', 'r') as file:
         return json.load(file)
+
 
 def check_updates(websites_dict):
     email_body = ""
@@ -20,42 +21,62 @@ def check_updates(websites_dict):
     for website_name, config in websites_dict.items():
         url = config['url']
         find_all_params = config['find_all_params']
-        
+
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
-        content_containers = soup.find_all(**find_all_params)
-        
-        new_content = ""
-        for container in content_containers:
-            new_content += f"{container.get_text(strip=True)}<br>"
 
-        # Check for updates.txt file and read its content
-        try:
-            with open("updates.txt", "r") as file:
-                old_content = file.read()
-        except FileNotFoundError:
-            old_content = ""  # If the file doesn't exist, consider it as empty
+        new_content = dict()
+        for params in find_all_params:
+            posting_blocks = soup.find_all(params['name'], class_=params['class'])
 
-        # Compare the old content with the new content
-        differ = difflib.Differ()
-        differences = list(differ.compare(old_content.splitlines(), new_content.splitlines()))
-        updates = [line for line in differences if line.startswith('+ ') or line.startswith('- ')]
+            for block in posting_blocks:
+                posting_id = block.get(params['job_data']['job_id']['attr'])
+                posting_name = block.find(
+                    params['job_data']['job_title']['name'],
+                    {
+                        params['job_data']['job_title']['attr']:
+                            params['job_data']['job_title']['value']
+                    }).get_text(strip=True)
+
+                new_content[posting_id] = posting_name
+
+        old_content = load_old_content(website_name)
+
+        # Compare old content with new
+        updates = {k: v for k, v in new_content.items() if old_content.get(k) != v}
 
         if updates:
             any_updates = True
-            update_text = '\n'.join(updates)
-            email_body += f"{website_name}\n{update_text}\n\n"
+            update_text = '<br><br>'.join(updates.values())
+            email_body += f"{website_name}<br><br>{update_text}<br>"
+
+        save_new_content(website_name, new_content)
+
+
+    subject = "Daily Job Posting Update"
+    sender = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+    recipients = os.getenv("EMAIL").split(",") if os.getenv("EMAIL") else [] # myself in this case
 
     if any_updates:
         send_email(subject, email_body, sender, recipients, password)
     else:
         send_email("Daily Job Posting Update - No Changes", "No updates found today.", sender, recipients, password)
 
-    subject = "Daily Job Posting Update"
-    sender = os.getenv("EMAIL")
-    password = os.getenv("PASSWORD")
-    recipients = os.getenv("EMAIL").split(",") if os.getenv("EMAIL") else [] # myself in this case
-    
+
+def load_old_content(website_name):
+    try:
+        with open(f"{website_name}_updates.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+
+def save_new_content(website_name, content):
+    with open(f"{website_name}_updates.json", "w") as file:
+        json.dump(content, file)
+
+
 def send_email(subject, body, sender, recipients, password):
     msg = MIMEText(body, 'html')
     msg['Subject'] = subject
@@ -66,9 +87,11 @@ def send_email(subject, body, sender, recipients, password):
         smtp_server.sendmail(sender, recipients, msg.as_string())
     print("Message sent!")
 
+
 def main():
     websites_dict = load_urls()
     check_updates(websites_dict)
+
 
 if __name__ == '__main__':
     main()
